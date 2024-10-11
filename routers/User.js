b3fb538290f4feadd2db28e281da67825230ca97
maelc <castellan.mael@efrei.net>
2024-10-11 12:36:12 -0400

    update route users

--- routers/User.js
+++ routers/User.js
@@ -7,13 +7,12 @@ const auth = require('../middleware/Authentification');
 const router = express.Router();
 
 
-router.use(auth.Auth);
 
-router.get('/', auth.admin,UserCtrl.GetAllUsers);
-router.get('/:id', auth.admin,UserCtrl.GetOneUser);
-router.post('/', auth.admin,UserCtrl.CreateUser);
-router.put('/:id', auth.admin,UserCtrl.UpdateUser);
-router.delete('/:id', auth.admin,UserCtrl.DeleteUser);
+router.get('/', auth.Auth,auth.admin,UserCtrl.GetAllUsers);
+router.get('/:id', auth.Auth,auth.admin,UserCtrl.GetOneUser);
+router.post('/', auth.Auth,auth.admin,UserCtrl.CreateUser);
+router.put('/:id',auth.Auth, auth.admin,UserCtrl.UpdateUser);
+router.delete('/:id',auth.Auth, auth.admin,UserCtrl.DeleteUser);
 
 router.post('/login', CustomerCtrl.Login);
 
